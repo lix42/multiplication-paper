9b7119ccc756128ea4f1b4d9f19868f8d1be89af
Li Xu <lix@xuli.dev>
2020-12-23 13:04:26 -0800

    print quizzes

--- src/quizzes/quizzesGenerator.ts
+++ src/quizzes/quizzesGenerator.ts
@@ -12,19 +12,20 @@ const isNum = (n?: number): n is Num =>
   n != null && Number.isFinite(n) && n >= minNum && n <= maxNum;
 
 const frequency: { [key in Num]: number } = {
-  2: 1,
-  3: 1,
-  4: 1,
-  5: 1,
-  6: 2,
-  7: 2,
-  8: 2,
-  9: 2,
+  2: 4,
+  3: 6,
+  4: 6,
+  5: 4,
+  6: 8,
+  7: 8,
+  8: 8,
+  9: 8,
   10: 1,
-  11: 1,
-  12: 1,
+  11: 3,
+  12: 3,
 };
-export const generateAllQuizzes = (): Quiz[] => {
+
+export const generateAllQuizzes = (count = 1): Quiz[] => {
   let result: Quiz[] = [];
   for (let first = minNum; first <= maxNum; first++) {
     for (let second = minNum; second <= first; second++) {
@@ -34,6 +35,9 @@ export const generateAllQuizzes = (): Quiz[] => {
         }
       }
     }
+  }
+  while (result.length < count) {
+    result = [...result, ...result];
   }
   return result;
 };
@@ -50,10 +54,25 @@ export const pickQuizzes = (candidates: Quiz[], count: number): Quiz[] => {
   }, []);
 };
 
-export const randomSortQuiz = (quiz: Quiz): Quiz =>
+export const shuffleQuizzes = (quizzes: Quiz[]): Quiz[] => {
+  const scope = quizzes.length * 100;
+  return quizzes
+    .map((quiz) => ({ ...quiz, order: Math.random() * scope }))
+    .sort((a, b) => a.order - b.order)
+    .map(({ order, ...quiz }) => quiz);
+};
+
+export const shakeQuiz = (quiz: Quiz): Quiz =>
   Math.random() * 2 > 1
     ? quiz
     : {
         first: quiz.second,
         second: quiz.first,
       };
+
+export const prepareQuizzes = (count: number) => {
+  const candidates = generateAllQuizzes(count);
+  const picked = pickQuizzes(candidates, count);
+  const shuffled = shuffleQuizzes(picked);
+  return shuffled.map(shakeQuiz);
+};
