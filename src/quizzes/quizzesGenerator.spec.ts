3aca6e93590fc092224a35990d776e4b995a54a7
Li Xu <lix@xuli.dev>
2020-12-23 11:20:51 -0800

    quiz generator

---
+++ src/quizzes/quizzesGenerator.spec.ts
@@ -0,0 +1,37 @@
+import { generateAllQuizzes, pickQuizzes, Quiz } from "./quizzesGenerator";
+
+describe("generateAllQuizzes", () => {
+  it("should return all test cases", () => {
+    const quizzes = generateAllQuizzes();
+    expect(quizzes).toMatchSnapshot();
+  });
+});
+
+describe("pickQuizzes", () => {
+  const candidates: Quiz[] = [
+    {
+      first: 2,
+      second: 2,
+    },
+    {
+      first: 3,
+      second: 3,
+    },
+    {
+      first: 4,
+      second: 4,
+    },
+  ];
+  it("should return an empty array if target count is 0", () => {
+    const result = pickQuizzes(candidates, 0);
+    expect(result).toEqual([]);
+  });
+  it("should return random quizzes", () => {
+    for (let i = 1; i <= 3; i++) {
+      const result = pickQuizzes(candidates, i);
+      expect(result).toHaveLength(i);
+      const set = new Set(result);
+      expect(set.size).toBe(i);
+    }
+  });
+});
