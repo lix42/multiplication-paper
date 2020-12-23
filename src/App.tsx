f0216c3bc036521b3eea1f55f5f41f360ed99685
Li Xu <lix@xuli.dev>
2020-12-23 13:04:26 -0800

    print quizzes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+import { prepareQuizzes } from "./quizzes/quizzesGenerator";
 
+const INITIAL_COUNT = 26;
 function App() {
+  const [count, setCount] = useState(INITIAL_COUNT);
+  const [quizzes, setQuizzes] = useState(() => prepareQuizzes(count));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="controller-bar">
+        <label>
+          How many questions?
+          <input
+            className="count-input"
+            type="number"
+            value={count}
+            min={4}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              const newCount = parseInt(e.target.value);
+              setCount(newCount);
+              setQuizzes(prepareQuizzes(newCount));
+            }}
+          />
+        </label>
+        <button type="button" onClick={() => setQuizzes(prepareQuizzes(count))}>
+          Refresh
+        </button>
+      </div>
+      <div className="quizzes-container">
+        {quizzes.map((quiz) => (
+          <span className="quiz">
+            {quiz.first} X {quiz.second} =
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
